0d7ec9e6b87d6fb09d3400d6c47e4de6fda8af66
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-09-02 10:52:47 +0200

    Initial Commit

---
+++ AccessibilityElementsHiddenPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessibilityElementsHiddenPOC
+//
+//  Created by MDJ on 02/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
