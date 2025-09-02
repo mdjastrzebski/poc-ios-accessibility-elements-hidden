0ee1be453d71287ff0674c711fd3ecddf4d3bea9
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-09-02 12:40:32 +0200

    POC

--- AccessibilityElementsHiddenPOC/ViewController.swift
+++ AccessibilityElementsHiddenPOC/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emptyView: UIView!
+    @IBOutlet weak var viewWithContent: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func onHiddenChanged(_ sender: UISwitch) {
+        set(accessibilityElementsHidden: sender.isOn)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        set(accessibilityElementsHidden: true);
     }
-
-
+    
+    func set(accessibilityElementsHidden hidden: Bool) {
+        emptyView.accessibilityElementsHidden = hidden;
+        viewWithContent.accessibilityElementsHidden = hidden;
+        label.accessibilityElementsHidden = hidden;
+        textView.accessibilityElementsHidden = hidden;
+        button.accessibilityElementsHidden = hidden;
+        textField.accessibilityElementsHidden = hidden;
+        imageView.accessibilityElementsHidden = hidden;
+    }
 }
 
